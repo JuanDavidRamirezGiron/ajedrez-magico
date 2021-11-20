083afb475f527e313271b9a40d91da1a48df54aa
Juan David Ramírez Girón <1534207@uab.cat>
2021-11-20 09:36:20 +0100

    Working with merges

--- Mesh.cpp
+++ Mesh.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "Mesh.h"
 Mesh::Mesh(std::vector <Vertex>& vertices, std::vector <GLuint>& indices, std::vector <Texture>& textures)
 {
@@ -20,7 +19,7 @@ Mesh::Mesh(std::vector <Vertex>& vertices, std::vector <GLuint>& indices, std::v
 }
 
 
-void Mesh::Draw( Shader& shader,Camera& camera, glm::vec3 translation, GLfloat rdeg, glm::vec3 raxis, glm::vec3 escalation)
+void Mesh::Draw(Shader& shader, Camera& camera, glm::vec3 translation, GLfloat rdeg, glm::vec3 raxis, glm::vec3 escalation)
 {
 	// Bind shader to be able to access uniforms
 	shader.Activate();
@@ -48,7 +47,7 @@ void Mesh::Draw( Shader& shader,Camera& camera, glm::vec3 translation, GLfloat r
 	// Take care of the camera Matrix
 	glUniform3f(glGetUniformLocation(shader.ID, "camPos"), camera.Position.x, camera.Position.y, camera.Position.z);
 	camera.Matrix(shader, "camMatrix");
-	
+
 
 	//Translation matrix (glm::vec3(0.0f, 0.0f, 0.0f))
 	glm::mat4 myTranslationMatrix = glm::translate(glm::mat4(1.0f), translation);
@@ -62,61 +61,5 @@ void Mesh::Draw( Shader& shader,Camera& camera, glm::vec3 translation, GLfloat r
 
 	glUniformMatrix4fv(glGetUniformLocation(shader.ID, "model"), 1, GL_FALSE, glm::value_ptr(myModelMatrix));
 	// Draw the actual mesh
-	glDrawArrays(GL_TRIANGLES, 0, sizeof(GLfloat)*vertices.size());
-=======
-#include "Mesh.h"
-
-Mesh::Mesh(std::vector <Vertex>& vertices, std::vector <GLuint>& indices, std::vector <Texture>& textures)
-{
-	Mesh::vertices = vertices;
-	Mesh::indices = indices;
-	Mesh::textures = textures;
-
-	VAO.Bind();
-	// Generates Vertex Buffer Object and links it to vertices
-	VBO VBO(vertices);
-	// Links VBO attributes such as coordinates and colors to VAO
-	VAO.LinkAttrib(VBO, 0, 3, GL_FLOAT, sizeof(Vertex), (void*)0);
-	VAO.LinkAttrib(VBO, 1, 3, GL_FLOAT, sizeof(Vertex), (void*)(3 * sizeof(float)));
-	VAO.LinkAttrib(VBO, 2, 3, GL_FLOAT, sizeof(Vertex), (void*)(6 * sizeof(float)));
-	VAO.LinkAttrib(VBO, 3, 2, GL_FLOAT, sizeof(Vertex), (void*)(9 * sizeof(float)));
-	// Unbind all to prevent accidentally modifying them
-	VAO.Unbind();
-	VBO.Unbind();
-}
-
-
-void Mesh::Draw( Shader& shader,Camera& camera)
-{
-	// Bind shader to be able to access uniforms
-	shader.Activate();
-	VAO.Bind();
-
-	// Keep track of how many of each type of textures we have
-	unsigned int numDiffuse = 0;
-	unsigned int numSpecular = 0;
-
-	for (unsigned int i = 0; i < textures.size(); i++)
-	{
-		std::string num;
-		std::string type = textures[i].type;
-		if (type == "diffuse")
-		{
-			num = std::to_string(numDiffuse++);
-		}
-		else //if (type == "specular")
-		{
-			num = std::to_string(numSpecular++);
-		}
-		textures[i].texUnit(shader, (type + num).c_str(), i);
-		textures[i].Bind();
-	}
-	// Take care of the camera Matrix
-	glUniform3f(glGetUniformLocation(shader.ID, "camPos"), camera.Position.x, camera.Position.y, camera.Position.z);
-	camera.Matrix(shader, "camMatrix");
-	glm::mat4 matrix = glm::mat4(1.0f);
-	glUniformMatrix4fv(glGetUniformLocation(shader.ID, "model"), 1, GL_FALSE, glm::value_ptr(matrix));
-	// Draw the actual mesh
-	glDrawArrays(GL_TRIANGLES, 0, sizeof(GLfloat)*vertices.size());
->>>>>>> origin/dev-G
+	glDrawArrays(GL_TRIANGLES, 0, sizeof(GLfloat) * vertices.size());
 }
--- Mesh.h
+++ Mesh.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MESH_CLASS_H
 #define MESH_CLASS_H
 
@@ -26,39 +25,9 @@ public:
 		Shader& shader,
 		Camera& camera,
 		glm::vec3 translation,
-		GLfloat rdeg, 
+		GLfloat rdeg,
 		glm::vec3 raxis,
 		glm::vec3 escalation
 	);
 };
-=======
-#ifndef MESH_CLASS_H
-#define MESH_CLASS_H
-
-#include<string>
-
-#include"VAO.h"
-#include"Camera.h"
-#include"Texture.h"
-
-class Mesh
-{
-public:
-	std::vector <Vertex> vertices;
-	std::vector <GLuint> indices;
-	std::vector <Texture> textures;
-	// Store VAO in public so it can be used in the Draw function
-	VAO VAO;
-
-	// Initializes the mesh
-	Mesh(std::vector <Vertex>& vertices, std::vector <GLuint>& indices, std::vector <Texture>& textures);
-
-	// Draws the mesh
-	void Draw
-	(
-		Shader& shader,
-		Camera& camera
-	);
-};
->>>>>>> origin/dev-G
 #endif
