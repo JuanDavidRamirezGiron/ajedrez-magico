78e63ed1f57b39320e30759b5c8ce78f94bb7f98
Juan David Ramírez Girón <1534207@uab.cat>
2021-12-13 00:40:53 +0100

    Raw autoplay

--- main.cpp
+++ main.cpp
@@ -7,9 +7,17 @@
 #include <cstdio>
 #include "Model.h"
 #include "Menu.h"
+#include <chrono>
+#include <thread>
 
 
 using namespace glm;
+
+void setOpenGLVersion();
+void updateBoard(GLFWwindow* window, int& i, int size, bool& released, vector<vec3> controlPoints, bool autoPlay);
+void drawPieces(GLFWwindow* window, Shader shaderProgram, Camera camera, Model board, Model pawn, Model bishop, Model tower, Model horse, Model queen, Model king, std::vector<std::vector<int>> arraygame, vector<vec3> controlPoints);
+vector<vec3> draw_TFBSpline_Curve(vector<vec3> ctr_points, int nptsCorba, float pas);
+vec3 Punt_Corba_BSpline(float t, vec3* ctr);
 
 //piezas
 #define PAWN 1
@@ -40,216 +48,22 @@ vector<vec3> punts = { {0,0,0}, {0,0,0} };
 const unsigned int width = 1920; //1280
 const unsigned int height = 1080; //720
 
-// Punt_Corba_Spline: Calcul del punt del spline en coordenades 3D (CPunt3D) segons el 
-//             paràmetre i i els punts de control ctr 
-vec3 Punt_Corba_BSpline(float t, vec3* ctr)
-{
-	// Matriu dels Splines
-	const double AS[4][4] =
-	{
-		{ -1.0 / 6.0, 0.5, -0.5, 1.0 / 6.0 },
-		{ 0.5, -1.0, 0.0, 4.0 / 6.0 },
-		{ -0.5, 0.5, 0.5, 1.0 / 6.0 },
-		{ 1.0 / 6.0, 0.0, 0.0, 0.0 }
-	};
-
-	vec3 p = { 0, 0, 0 };
-	float coef[4];
-	int i, j;
-
-	// Polinomis que multipliquen els punts de control del patch
-	for (i = 0; i < 4; i++)
-	{
-		coef[i] = 0;
-		for (j = 0; j < 4; j++)
-			coef[i] = coef[i] * t + AS[i][j];
-	}
-
-	// Càlcul de la Posició
-	for (i = 0; i < 4; i++)
-	{
-		p.x += coef[i] * ctr[i].x;
-		p.y += coef[i] * ctr[i].y;
-		p.z += coef[i] * ctr[i].z;
-	}
-	
-	p.x *= 5;
-	p.y *= 5;
-	p.z *= 5;
-	
-	
-	return p;
-
-}
-
-vector<vec3> draw_TFBSpline_Curve(vector<vec3> ctr_points, int nptsCorba, float pas)
-{
-	vec3 vertexL1, vertexL2;
-	vec3 ctr[4];		// Punts control del patch de la B-Spline.
-	int patch = 0;		// Patch actual.
-	GLfloat t = 0;
-	vector<vec3> puntsCorba;
-
-	//t = t - pas;
-// Càrrega primers punts de control.
-	for (int i = 0; i < 4; i++)
-	{
-		float aux = ctr_points[i].y;
-		ctr_points[i].y = ctr_points[i].z;
-		ctr_points[i].z = -aux;
-
-		ctr[i].x = ctr_points[i].x;
-		ctr[i].y = ctr_points[i].y;
-		ctr[i].z = ctr_points[i].z;
-
-	}
-
-
-
-	// Càlcul i dibuix Triedre de Frenet en cada vèrtex de la corba B-Spline
-	vertexL1 = Punt_Corba_BSpline(t, ctr);
-
-	t = t + pas;
-	while (patch <= nptsCorba - 4) {
-		if (t >= 1.0)
-		{
-			vertexL2 = Punt_Corba_BSpline(1.0, ctr);
-			puntsCorba.push_back(vertexL2);
-			t = 0.0;
-			patch++;
-			if (patch <= nptsCorba - 4)
-			{
-				for (int i = 0; i < 4; i++)
-				{
-					ctr[i].x = ctr_points[patch + i].x;
-					ctr[i].y = ctr_points[patch + i].y;
-					ctr[i].z = ctr_points[patch + i].z;
-				}
-			}
-		}
-		else if (patch <= nptsCorba - 4) {
-			vertexL2 = Punt_Corba_BSpline(t, ctr);
-			puntsCorba.push_back(vertexL2);
-			vertexL1 = vertexL2;
-			t = t + pas;
-		}
-	}
-	return puntsCorba;
-}
-
-void drawPieces(GLFWwindow* window, Shader shaderProgram, Camera camera, Model board, Model pawn, Model bishop, Model tower, Model horse, Model queen, Model king, std::vector<std::vector<int>> arraygame, vector<vec3> controlPoints) {
-
-	const int originX = -9;
-	const int originZ = 10;
-	const int cellSize = 2;
-	vec3 pieceZoom = vec3(0.2f, 0.2f, 0.2f);
-	vec3 pieceRotation = vec3(1.f, 0.f, 0.f);
-
-	//Dibujamos el tablero
-	board.Draw(shaderProgram, camera, vec3(0.0f, -1.9f, 0.0f), 1.5708f, vec3(1.f, 0.f, 0.f), vec3(1.f, 1.f, 1.f));
-
-	//Iteramos en el vector de posiciones de piezas
-	for (auto piece : arraygame) {
-		//aqui codigo para pasar del vector a las posiciones
-		int pieceX = originX + piece[2] * cellSize;
-		int pieceZ = originZ - piece[3] * cellSize;
-		
-		if (pieceX == controlPoints[3].x && pieceZ == controlPoints[3].z)
-		{
-			
-			translationPiece = piece;
-			continue;
-		}
-		switch (piece[0])
-		{
-		case PAWN:
-			pawn.Draw(shaderProgram, camera, vec3(pieceX, 0.0f, pieceZ), 0.0f, pieceRotation, pieceZoom);
-			break;
-		case BISHOP:
-			bishop.Draw(shaderProgram, camera, vec3(pieceX, 0.0f, pieceZ), 0.0f, pieceRotation, pieceZoom);
-			break;
-		case TOWER:
-			tower.Draw(shaderProgram, camera, vec3(pieceX, 0.0f, pieceZ), 0.0f, pieceRotation, pieceZoom);
-			break;
-		case HORSE:
-			horse.Draw(shaderProgram, camera, vec3(pieceX, 0.0f, pieceZ), 00.f, pieceRotation, pieceZoom);
-			break;
-		case QUEEN:
-			queen.Draw(shaderProgram, camera, vec3(pieceX, 0.0f, pieceZ), 0.0f, pieceRotation, pieceZoom);
-			break;
-		case KING:
-			king.Draw(shaderProgram, camera, vec3(pieceX, 0.0f, pieceZ), 0.0f, pieceRotation, pieceZoom);
-			break;
-		default:
-			break;
-		}
-	}
-
-	
-
-}
-
-void updateBoard(GLFWwindow* window, int& i, int size, bool& released, vector<vec3> controlPoints) {
-	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS && i < size - 1 && released)
-	{
-		released = false;
-		i++;
-	}
-	else if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS && i > 0 && released)
-	{
-		released = false;
-		i--;
-	}
-	else if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_RELEASE && !released && glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_RELEASE)
-	{
-		punts = draw_TFBSpline_Curve(controlPoints, 4, 0.005);
-		released = true;
-	}
-}
-
-void setOpenGLVersion() {
-
-	//Inicializar GLFW y Opengl version 3.3
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-}
-
-void shaderloader() {
-
-}
-
 int main() {
 
 	SetConsoleOutputCP(CP_UTF8);
 	setvbuf(stdout, nullptr, _IOFBF, 1000);
-
-	cout << "Empieza la lectura del programa" << endl;
-
-	//PGN Reader, not finished
-	/*PGNFileReader* reader = new PGNFileReader("Jugadas.pgn");
-
-	map<int, pair<string, string>> plays;
-
-	plays = reader->gatherInformation();*/
-
-	//FEN files reader
-	FENFileReader* reader = new FENFileReader("Jugadas.fen");
-	Movements* reader1 = new Movements;
-
+	
 	vector<vector<vector<char>>> allBoardStatus;
-
-	//FENReader returns all boards read form the file
-	allBoardStatus = reader->prepareBoard();
-
-	StatusToPlaysTransformer* transformer = new StatusToPlaysTransformer();
 	vector<vector<vector<int>>> allPlays;
-	allPlays = transformer->transformStatusToPlays(allBoardStatus);
-
 	vector<vec3> controlPoints;
 
+	Movements* reader1 = new Movements;
+	FENFileReader* reader = new FENFileReader("Jugadas.fen");
+	StatusToPlaysTransformer* transformer = new StatusToPlaysTransformer();
+	
+	allBoardStatus = reader->prepareBoard();
+	allPlays = transformer->transformStatusToPlays(allBoardStatus);
+	
 	setOpenGLVersion();
 
 	//creamos ventana
@@ -337,6 +151,8 @@ int main() {
 	double previousTime = glfwGetTime();
 	controlPoints = reader1->compareBoards(allBoardStatus, currentPlay);
 
+	bool autoPlay = true;
+
 	while (!glfwWindowShouldClose(window)) {
 
 		// Ajustamos color barrido
@@ -395,7 +211,8 @@ int main() {
 		pawn.Draw(shaderProgram, camera, vec3(pieceX, pieceY, pieceZ), 0.0f, pieceRotation, pieceZoom);
 
 		// actualizacion e intercambio de buffers
-		updateBoard(window, currentPlay, allPlays.size(), released, controlPoints);
+		Sleep(1000);
+		updateBoard(window, currentPlay, allPlays.size(), released, controlPoints, autoPlay);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
@@ -407,5 +224,194 @@ int main() {
 	glfwTerminate();
 	return 0;
 	
+}
+
+
+// Punt_Corba_Spline: Calcul del punt del spline en coordenades 3D (CPunt3D) segons el 
+//             paràmetre i i els punts de control ctr 
+vec3 Punt_Corba_BSpline(float t, vec3* ctr)
+{
+	// Matriu dels Splines
+	const double AS[4][4] =
+	{
+		{ -1.0 / 6.0, 0.5, -0.5, 1.0 / 6.0 },
+		{ 0.5, -1.0, 0.0, 4.0 / 6.0 },
+		{ -0.5, 0.5, 0.5, 1.0 / 6.0 },
+		{ 1.0 / 6.0, 0.0, 0.0, 0.0 }
+	};
+
+	vec3 p = { 0, 0, 0 };
+	float coef[4];
+	int i, j;
+
+	// Polinomis que multipliquen els punts de control del patch
+	for (i = 0; i < 4; i++)
+	{
+		coef[i] = 0;
+		for (j = 0; j < 4; j++)
+			coef[i] = coef[i] * t + AS[i][j];
+	}
+
+	// Càlcul de la Posició
+	for (i = 0; i < 4; i++)
+	{
+		p.x += coef[i] * ctr[i].x;
+		p.y += coef[i] * ctr[i].y;
+		p.z += coef[i] * ctr[i].z;
+	}
+
+	p.x *= 5;
+	p.y *= 5;
+	p.z *= 5;
+
+
+	return p;
+
+}
+
+vector<vec3> draw_TFBSpline_Curve(vector<vec3> ctr_points, int nptsCorba, float pas)
+{
+	vec3 vertexL1, vertexL2;
+	vec3 ctr[4];		// Punts control del patch de la B-Spline.
+	int patch = 0;		// Patch actual.
+	GLfloat t = 0;
+	vector<vec3> puntsCorba;
+
+	//t = t - pas;
+// Càrrega primers punts de control.
+	for (int i = 0; i < 4; i++)
+	{
+		float aux = ctr_points[i].y;
+		ctr_points[i].y = ctr_points[i].z;
+		ctr_points[i].z = -aux;
+
+		ctr[i].x = ctr_points[i].x;
+		ctr[i].y = ctr_points[i].y;
+		ctr[i].z = ctr_points[i].z;
+
+	}
+
+
+
+	// Càlcul i dibuix Triedre de Frenet en cada vèrtex de la corba B-Spline
+	vertexL1 = Punt_Corba_BSpline(t, ctr);
+
+	t = t + pas;
+	while (patch <= nptsCorba - 4) {
+		if (t >= 1.0)
+		{
+			vertexL2 = Punt_Corba_BSpline(1.0, ctr);
+			puntsCorba.push_back(vertexL2);
+			t = 0.0;
+			patch++;
+			if (patch <= nptsCorba - 4)
+			{
+				for (int i = 0; i < 4; i++)
+				{
+					ctr[i].x = ctr_points[patch + i].x;
+					ctr[i].y = ctr_points[patch + i].y;
+					ctr[i].z = ctr_points[patch + i].z;
+				}
+			}
+		}
+		else if (patch <= nptsCorba - 4) {
+			vertexL2 = Punt_Corba_BSpline(t, ctr);
+			puntsCorba.push_back(vertexL2);
+			vertexL1 = vertexL2;
+			t = t + pas;
+		}
+	}
+	return puntsCorba;
+}
+
+void drawPieces(GLFWwindow* window, Shader shaderProgram, Camera camera, Model board, Model pawn, Model bishop, Model tower, Model horse, Model queen, Model king, std::vector<std::vector<int>> arraygame, vector<vec3> controlPoints) {
+
+	const int originX = -9;
+	const int originZ = 10;
+	const int cellSize = 2;
+	vec3 pieceZoom = vec3(0.2f, 0.2f, 0.2f);
+	vec3 pieceRotation = vec3(1.f, 0.f, 0.f);
+
+	//Dibujamos el tablero
+	board.Draw(shaderProgram, camera, vec3(0.0f, -1.9f, 0.0f), 1.5708f, vec3(1.f, 0.f, 0.f), vec3(1.f, 1.f, 1.f));
+
+	//Iteramos en el vector de posiciones de piezas
+	for (auto piece : arraygame) {
+		//aqui codigo para pasar del vector a las posiciones
+		int pieceX = originX + piece[2] * cellSize;
+		int pieceZ = originZ - piece[3] * cellSize;
+
+		if (pieceX == controlPoints[3].x && pieceZ == controlPoints[3].z)
+		{
+
+			translationPiece = piece;
+			continue;
+		}
+		switch (piece[0])
+		{
+		case PAWN:
+			pawn.Draw(shaderProgram, camera, vec3(pieceX, 0.0f, pieceZ), 0.0f, pieceRotation, pieceZoom);
+			break;
+		case BISHOP:
+			bishop.Draw(shaderProgram, camera, vec3(pieceX, 0.0f, pieceZ), 0.0f, pieceRotation, pieceZoom);
+			break;
+		case TOWER:
+			tower.Draw(shaderProgram, camera, vec3(pieceX, 0.0f, pieceZ), 0.0f, pieceRotation, pieceZoom);
+			break;
+		case HORSE:
+			horse.Draw(shaderProgram, camera, vec3(pieceX, 0.0f, pieceZ), 00.f, pieceRotation, pieceZoom);
+			break;
+		case QUEEN:
+			queen.Draw(shaderProgram, camera, vec3(pieceX, 0.0f, pieceZ), 0.0f, pieceRotation, pieceZoom);
+			break;
+		case KING:
+			king.Draw(shaderProgram, camera, vec3(pieceX, 0.0f, pieceZ), 0.0f, pieceRotation, pieceZoom);
+			break;
+		default:
+			break;
+		}
+	}
+
+
+
+}
+
+void updateBoard(GLFWwindow* window, int& i, int size, bool& released, vector<vec3> controlPoints, bool autoPlay) {
+
+	if (autoPlay) {
+
+		i++;
+
+	}else {
+
+		if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS && i < size - 1 && released)
+		{
+			released = false;
+			i++;
+		}
+		else if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS && i > 0 && released)
+		{
+			released = false;
+			i--;
+		}
+		else if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_RELEASE && !released && glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_RELEASE)
+		{
+			punts = draw_TFBSpline_Curve(controlPoints, 4, 0.005);
+			released = true;
+		}
+
+	}
+
+	
+}
+
+void setOpenGLVersion() {
+
+	//Inicializar GLFW y Opengl version 3.3
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 }
 
